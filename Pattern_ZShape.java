50e22995c19884b435e8b052b4ddff1c61e496a7
SHJA AZIMI <110715621+SHJAx19@users.noreply.github.com>
2023-02-28 19:19:19 +0300

    Add files via upload

---
+++ Pattern_ZShape.java
@@ -0,0 +1,17 @@
+public class Pattern_ZShape {
+    public static void main(String[] args) {
+
+        int row = 5;
+
+        for (int i = 0; i < row; ++i) {
+            for (int j = 0; j < row; ++j)
+                if (i == 0 || i == row - 1 || (i + j) == row - 1)
+                    System.out.print("# ");
+                else
+                    System.out.print("  ");
+            System.out.println("");
+
+        }
+    }
+
+}
